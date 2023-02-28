021c6d59d24aab5f16f0e9e085cd04dbc9081db6
ibrahimaq <ibrahim_aq@hotmail.co.uk>
2023-02-28 22:13:26 +0000

    footer wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,14 @@ module.exports = {
         primary: "#FF52BF",
         secondary: "#808D99",
         cyanLight: "#EBFBFF",
-        dark: "#00242D",
+        dark: "#334155",
       },
       backgroundImage: {
         "hero-desktop": "url('/assets/bg-hero-desktop.svg')",
         "hero-mobile": "url('/assets/bg-hero-mobile.svg')",
-        'reviews-img': 'url("/assets/watercolor-4116932.png")'
+        "reviews-img": 'url("/assets/watercolor-4116932.png")',
+        radialGradient:
+          "radial-gradient(circle, rgba(255,82,191,0.07046568627450978) 17%, rgba(0,212,255,0.025647759103641476) 34%, rgba(235,251,255,1) 54%);",
       },
       fontSize: {
         bodyCopy: "18px",
@@ -22,8 +24,18 @@ module.exports = {
         public: ["Poppins", "sans-serif"],
       },
       padding: {
-        'x': '20px',
-      }
+        x: "20px",
+      },
+      margin: {
+        x: "20px",
+      },
+      height: {
+        "90vh": "90vh",
+        "40vh": "40vh",
+      },
+      boxShadow: {
+        custom: "2px -3px 8px -1px rgba(225,225,225,0.88);",
+      },
     },
   },
   plugins: [],
