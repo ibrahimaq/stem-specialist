b9ae7ace26e86a87d2761f1ae6f259af54580134
ibrahimaq <ibrahim_aq@hotmail.co.uk>
2023-03-04 03:19:51 +0000

    footer + review carousel

--- script.js
+++ script.js
@@ -1,68 +1,159 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav')
-const burgerTop = document.querySelector('.burger-top');
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+const burgerTop = document.querySelector(".burger-top");
 const burgerMiddle = document.querySelector(".burger-middle");
 const burgerBottom = document.querySelector(".burger-bottom");
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
 let mobileMenuActive = false;
 const toggleMenu = () => {
-    
-    console.log(mobileMenuActive)
-    if (!mobileMenuActive) {
-        mobileMenuActive = true;
-        nav.classList.remove("translate-x-[1000px]");
-        burgerTop.classList.add("-rotate-45", "origin-top-right");
+  if (!mobileMenuActive) {
+    mobileMenuActive = true;
+    nav.classList.remove("translate-x-[1000px]");
+    burgerTop.classList.remove("rotate-0", "origin-top-right", "w-full");
+    burgerTop.classList.add("-rotate-45", "origin-top-right", "w-[45px]");
 
-        // burgerMiddle.classList.remove('opacity-100');
-        // burgerMiddle.classList.add("opacity-0");
-        burgerMiddle.classList.remove("w-[20px]");
-        burgerMiddle.classList.add("w-0");
+    // burgerMiddle.classList.remove('opacity-100');
+    // burgerMiddle.classList.add("opacity-0");
+    burgerMiddle.classList.remove("w-1/2");
+    burgerMiddle.classList.add("w-0");
 
-        burgerBottom.classList.remove('w-[12px]', 'rotate-0', 'origin-bottom-right');
-        burgerBottom.classList.add(
-          "w-[30px]",
-          "rotate-45",
-          "origin-bottom-right",
-        );
+    burgerBottom.classList.remove("w-1/4", "rotate-0", "origin-bottom-right");
+    burgerBottom.classList.add("w-[45px]", "rotate-45", "origin-bottom-right");
+  } else {
+    mobileMenuActive = false;
+    burgerTop.classList.remove("-rotate-45", "origin-top-right", "w-[45px]");
+    burgerTop.classList.add("rotate-0", "origin-top-right", "w-full");
 
+    // burgerMiddle.classList.remove("opacity-0");
+    // burgerMiddle.classList.add('opacity-100');
+    burgerMiddle.classList.remove("w-0");
+    burgerMiddle.classList.add("w-1/2");
+    burgerBottom.classList.remove(
+      "w-[45px]",
+      "rotate-45",
+      "origin-bottom-right"
+    );
+    burgerBottom.classList.add("w-1/4", "rotate-0", "origin-bottom-right");
 
-    } else {
-        mobileMenuActive = false;
-        burgerTop.classList.remove("-rotate-45", 'origin-top-right');
-        burgerTop.classList.add('-rotate-0', 'origin-top-right');
-
-        // burgerMiddle.classList.remove("opacity-0");
-        // burgerMiddle.classList.add('opacity-100');
-        burgerMiddle.classList.remove("w-0");
-        burgerMiddle.classList.add("w-[20px]");
-        burgerBottom.classList.remove(
-          "w-[30px]",
-          "rotate-45",
-          "origin-bottom-right"
-        );
-        burgerBottom.classList.add(
-          "w-[12px]",
-          "rotate-0",
-          "origin-bottom-right"
-        );
-        
-        return nav.classList.add('translate-x-[1000px]')
-    }
-}
+    return nav.classList.add("translate-x-[1000px]");
+  }
+};
 
 burger.addEventListener("click", toggleMenu);
 
+// var width = window.innerWidth;
+// window.addEventListener('resize', () => {
+//   var width = window.innerWidth;
+//   console.log(width)
+// })
 
 const changeNavBg = () => {
-  console.log(window.scrollY)
-  console.log(header.classList.contains('md:bg-white/100'))
-  if (window.scrollY > 140) {
-    header.classList.add("md:bg-white/60", "md:backdrop-blur-lg");
-  } else {
-    header.classList.remove("md:bg-white/60", "md:backdrop-blur-lg");
+  if (scrollY > 140) {
+    header.classList.remove("bg-transparent");
+    header.classList.add("bg-white", "shadow-md");
+  } else if (scrollY <= 140 && header.classList.contains("bg-white")) {
+    header.classList.remove("bg-white", "shadow-md");
+    header.classList.add("bg-transparent");
   }
-}
 
-document.addEventListener('scroll', changeNavBg)
+  // if (window.innerWidth > 767) {
+  //   // from mobile to desktop
+  //   if (window.scrollY > 140 && header.classList.contains('bg-white')) {
+  //     header.classList.remove('bg-white')
+  //     header.classList.add("backdrop-blur-md", "bg-white/40");
+  //   } else if (window.scrollY > 140 && !header.classList.contains("bg-white")) {
+  //       header.classList.remove('bg-transparent');
+  //       header.classList.add("backdrop-blur-md", "bg-white/40");
+  //   }
+  // } else {
+  //   if (
+  //     window.scrollY > 140 &&
+  //     header.classList.contains("backdrop-blur-md", "bg-white/40")
+  //   ) {
+  //     header.classList.remove("backdrop-blur-md", "bg-white/40");
+  //     header.classList.add("bg-white");
+  //   } else if (
+  //     window.scrollY > 140 &&
+  //     !header.classList.contains("backdrop-blur-md", "bg-white/40")
+  //   ) {
+  //     header.classList.remove("bg-transparent");
+  //     header.classList.add("bg-white");
+  //   }
+  // }
+};
+
+document.addEventListener("scroll", changeNavBg);
+document.addEventListener("resize", changeNavBg);
+
+////// REVIEW CAROUSEL //////
+const slidesContainer = document.querySelector("#slides-container");
+const slide = document.querySelector(".slide");
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+
+const comment = document.querySelector(".comment");
+const author = document.querySelector(".author");
+
+const reviews = [
+  {
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquid omnis, necessitatibus ipsum dicta sunt facilis eligendi recusandae velit mollitia autem sit architecto ipsam a aut est repellat officia eos nostrum totam possimus harum! Alias",
+    author: "Becky A.",
+  },
+  {
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, eaque.",
+    author: "Adma B.",
+  },
+  {
+    comment:
+      "Idi recusandae velit mollitia autem sit architecto ipsam a aut est repellat officia eos nostrum totam possimus harum!",
+    author: "Barbara W.",
+  },
+];
+
+let i = 0;
+
+nextBtn.addEventListener("click", () => {
+  slide.classList.add("-translate-x-[100%]", "opacity-0");
+
+  if (i === reviews.length - 1) {
+    i = -1;
+  }
+  i += 1;
+
+  setTimeout(() => {
+    slide.classList.remove("-translate-x-[100%]");
+    slide.classList.add("translate-x-[100%]");
+  }, 100);
+
+  comment.textContent = reviews[i].comment;
+  author.textContent = reviews[i].author;
+
+  setTimeout(() => {
+    slide.classList.remove("translate-x-[100%]", "opacity-0");
+  }, 200);
+});
+
+prevBtn.addEventListener("click", () => {
+  slide.classList.add("translate-x-[100%]", "opacity-0");
+
+  if (i === 0) {
+    i = reviews.length;
+  }
+  i -= 1;
+
+  setTimeout(() => {
+    slide.classList.remove("translate-x-[100%]");
+    slide.classList.add("-translate-x-[100%]");
+  }, 100);
+
+  comment.textContent = reviews[i].comment;
+  author.textContent = reviews[i].author;
+
+  setTimeout(() => {
+    slide.classList.remove("-translate-x-[100%]", "opacity-0");
+  }, 200);
+});
